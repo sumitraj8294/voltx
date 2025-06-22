25e221c718c5c61557eed4e87929b084584e3b6f
sumitraj8294 <sumitraj8294@gmail.com>
2025-06-22 08:19:32 +0530

    Fixed the map zoom issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
+import 'leaflet/dist/leaflet.css';
 
 import Home from './pages/Home';
 import About from './pages/About';
