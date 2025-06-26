cd0e0c1138f4ad58e1dfeb4e3f1d2bb44a609dfc
Sumit raj <95587332+sumitraj8294@users.noreply.github.com>
2025-06-26 13:34:07 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', formData);
+      const res = await axios.post('https://voltx-ev-charge.onrender.com/api/auth/login', formData);
       
       login(res.data); // ✅ update context state and localStorage here
       alert('Login successful!');
