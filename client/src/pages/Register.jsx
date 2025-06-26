e4157f32b68e06be9bd133d17a113235ca0c1ce6
Sumit raj <95587332+sumitraj8294@users.noreply.github.com>
2025-06-26 13:33:31 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -30,7 +30,7 @@ const Register = () => {
     }
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/register', {
+      const res = await axios.post('https://voltx-ev-charge.onrender.com/api/auth/register', {
         name,
         email,
         password,
